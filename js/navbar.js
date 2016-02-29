94722f280e8762c349d80ae693218a6cb0c6c5a9
Sakari Pesonen <sakari.pesonen@eficode.com>
2016-02-29 16:18:06 +0200

    new colors and separators

--- js/navbar.js
+++ js/navbar.js
@@ -3,16 +3,26 @@
     
 	// hide .navbar first
 	$(".navbar").hide();
+
 	
 	// fade in .navbar
 	$(function () {
+		var previousTop = 0
 		$(window).scroll(function () {
             // set distance user needs to scroll before we fadeIn navbar
-			if ($(this).scrollTop() > 100) {
-				$('.navbar').fadeIn();
-			} else {
-				$('.navbar').fadeOut();
-			}
+			// if ($(this).scrollTop() > jee) {
+			// 	$('.navbar').fadeIn();
+			// } else {
+			// 	$('.navbar').fadeOut();
+			// }
+			var currentTop = $(window).scrollTop();
+	    	if (currentTop < previousTop) {
+	        	$('.navbar').fadeIn();
+	    	} else {
+	        	$('.navbar').fadeOut();
+	    	}
+	    	previousTop = currentTop;
+
 		});
 
 	
